548e0e0f3f0bf20ce9f0fcde0b8bdf15f4eaf6aa
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-25 14:08:11 -0300

    cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const uuid = require("uuid");
+
+const acount = [];
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
   return res.json(["Curson 1", "Curson 2", "Curson 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
   return res.json(["Curson 1", "Curson 2", "Curson 3"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
   return res.json(["Curson 1", "Curson 2", "Curson 3"]);
 });
 
@@ -20,6 +28,19 @@ app.patch("/courses/:id", (req, res) => {
 
 app.delete("/courses/:id", (req, res) => {
   return res.json(["Curson 1", "Curson 2", "Curson 3"]);
+});
+
+app.post("/account", (req, res) => {
+  const newUser = { ...req.body, id: uuid.v4(), statement: [] };
+  if (acount.some((user) => user.cpf === newUser.cpf))
+    return res.status(400).json({
+      msg: "cpf already in use",
+    });
+  acount.push(newUser);
+  return res.status(201).json({
+    msg: "created",
+    id: newUser.id,
+  });
 });
 
 app.listen(3333);
