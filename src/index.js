3dc1bb0ab3671c7513ac62f2b2b3f71274013d1d
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-25 11:55:01 -0300

    utilizando os metodos http

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({
-    msg: "fundamentos node js! ",
-  });
+app.get("/courses", (req, res) => {
+  return res.json(["Curson 1", "Curson 2", "Curson 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json(["Curson 1", "Curson 2", "Curson 3"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Curson 1", "Curson 2", "Curson 3"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curson 1", "Curson 2", "Curson 3"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curson 1", "Curson 2", "Curson 3"]);
 });
 
 app.listen(3333);
