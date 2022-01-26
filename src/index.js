98c803ae19d8a017962a90606d8db0609e59c3b9
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-26 13:18:30 -0300

    atualizar conta

--- src/index.js
+++ src/index.js
@@ -129,4 +129,24 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
   });
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body;
+
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(200).json({
+    msg: "atualizado com sucesso",
+  });
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  return res.status(200).json({
+    ...customer,
+  });
+});
+
 app.listen(3333);
